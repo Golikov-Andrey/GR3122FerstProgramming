fb4fcdb3742d29c1d5e4b2fd9171503c34eb00d7
Golikov-Andrey <newsc2@yandex.ru>
2022-09-03 15:00:12 +0300

    add task 9,11,12,14

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,25 +1,57 @@
-﻿//=============================================================================
-// # 0 Напишите программу, которая на вход принимает число и
-// выдаёт его квадрат (число умноженное на само себя).
-//=============================================================================
+﻿// //=============================================================================
+// // # 0 Напишите программу, которая на вход принимает число и
+// // выдаёт его квадрат (число умноженное на само себя).
+// //=============================================================================
 
-string? inputLine = Console.ReadLine();
-if (inputLine != null)
+// string? inputLine = Console.ReadLine();
+// if (inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+//     int outNumber = 0;
+
+//     DateTime d1 = DateTime.Now;
+//     for (int i = 0; i < 10000000; i++)
+//     {
+//         outNumber = inputNumber * inputNumber;
+//     }
+
+//     DateTime d2 = DateTime.Now;
+//     Console.WriteLine(d2 - d1);
+
+
+//     //int outNumber = (int)Math.Pow(inputNumber,2);
+
+//     Console.WriteLine(outNumber);
+// }
+
+
+// ====================================================================
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// ============================================================================
+
+// First way
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int i = 2;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + num);
+if (num < 2)
 {
-    int inputNumber = int.Parse(inputLine);
-    int outNumber = 0;
-
-    DateTime d1 = DateTime.Now;
-    for (int i = 0; i < 10000000; i++)
+    Console.WriteLine("Нет чётных чисел!");
+}
+else
+{
+    while (i <= num - 2)
     {
-        outNumber = inputNumber * inputNumber;
+        Console.Write(i + ", ");
+        i += 2;
     }
-
-    DateTime d2 = DateTime.Now;
-    Console.WriteLine(d2 - d1);
-
-
-    //int outNumber = (int)Math.Pow(inputNumber,2);
-
-    Console.WriteLine(outNumber);
+    Console.Write(i);
 }
